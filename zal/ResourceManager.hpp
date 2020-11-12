26dc07425060e7362e96c4e5918991c6a466f880
zakfilip <70323056+zakfilip@users.noreply.github.com>
2020-11-12 15:44:09 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -1,8 +1,45 @@
 #pragma once
 
 #include "Resource.hpp"
+using namespace std;
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+	ResourceManager()
+	{
+		x = 0;
+	}
+	ResourceManager(ResourceManager& r)
+	{
+		x = r.get();
+	}
+	ResourceManager& operator=(ResourceManager& r)
+	{
+		delete x;
+		x = r.get();
+		return r;
+	}
+	ResourceManager& operator=(ResourceManager&& r)
+	{
+		delete x;
+		x = r.x;
+		r.x = nullptr;
+		return *this;
+	}
+	ResourceManager(ResourceManager&& r)
+	{
+		x = r.x;
+		r.x = nullptr;
+	}
+	~ResourceManager()
+	{
+		delete x;
+	}
+	double* get()
+	{
+		return x;
+	}
+private:
+	double* x;
 };
